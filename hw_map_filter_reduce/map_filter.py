3f9b9820bef5f2e12ef78ca83597802b14087cfc
vadym-khodak <vaddimart@gmail.com>
2022-01-04 23:31:46 +0200

    add hw_map_filter_reduce

---
+++ hw_map_filter_reduce/map_filter.py
@@ -0,0 +1,22 @@
+"""
+Написати функцію, яка приймає послідовність повних імен та повертає
+ту саму послідовність імен, де всі імена та прізвища починаються з великих літер,
+а решта літер маленькі та відсутні зайві пробіли з початку та в кінці повного ім’я
+та лишає тільки один пробіл між ім’ям та прізвищем. Використовувати map() та filter().
+
+Приклад:
+>>> format_names([" joHn    DOE  ", " BRIan    smiTh  "])
+["John Doe", "Brian Smith"]
+"""
+
+
+def format_names(names: list[str]) -> list[str]:
+    return list(map(lambda name: " ".join(filter(lambda i: i, name.strip().title().split())), names))
+
+
+print(format_names([" joHn     DOE  ", " BRIan     smiTh  "]))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
